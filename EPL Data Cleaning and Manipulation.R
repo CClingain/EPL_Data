20a66a31c98835c16fa7c3b993ca81c04ee7c1e6
CClingain <36608104+CClingain@users.noreply.github.com>
2018-03-04 13:37:26 -0500

    Updated with correct season/year columns (dates changed)

--- EPL Data Cleaning and Manipulation.R
+++ EPL Data Cleaning and Manipulation.R
@@ -23,16 +23,20 @@ library(dplyr)
 #Not interested in the betting odds, so going to remove those; also don't need division (all EPL)
 names(s17.18)
 s17.18 <- s17.18[,2:23]
+s17.18$Date <- as.Date(s17.18$Date, format = "%d/%m/%y")
 
 #/2016-2017
 s16.17 <- read.csv("2016-2017.csv")
 names(s16.17) #always check the names! order can change
 s16.17 <- s16.17[,2:23]
+s16.17$Date <- as.Date(s16.17$Date, format = "%d/%m/%y")
 
 #/2015-2016
 s15.16 <- read.csv("2015-2016.csv")
 names(s15.16)
 s15.16 <- s15.16[,2:23]
+s15.16$Date <- as.Date(s15.16$Date, format = "%d/%m/%y")
+
 
 #/2014-2015
 s14.15 <- read.csv("2014-2015.csv")
@@ -40,81 +44,100 @@ names(s14.15)
 s14.15 <- s14.15[,2:23]
 #remove last empty row
 s14.15 <- s14.15[-381,]
+s14.15$Date <- as.Date(s14.15$Date, format = "%d/%m/%y")
+
 
 #/2013-2014
 s13.14 <- read.csv("2013-2014.csv")
 names(s13.14)
 s13.14 <- s13.14[,2:23]
+s13.14$Date <- as.Date(s13.14$Date, format = "%d/%m/%y")
+
 
 #/2012-2013
 s12.13 <- read.csv("2012-2013.csv")
 names(s12.13)
 s12.13 <- s12.13[,2:23]
+s12.13$Date <- as.Date(s12.13$Date, format = "%d/%m/%y")
+
 
 #/2011-2012
 s11.12 <- read.csv("2011-2012.csv")
 names(s11.12)
 s11.12 <- s11.12[,2:23]
+s11.12$Date <- as.Date(s11.12$Date, format = "%d/%m/%y")
 
 #/2010-2011
 s10.11 <- read.csv("2010-2011.csv")
 names(s10.11)
 s10.11 <- s10.11[,2:23]
+s10.11$Date <- as.Date(s10.11$Date, format = "%d/%m/%y")
 
 #/2009-2010
 s09.10 <- read.csv("2009-2010.csv")
 names(s09.10)
 s09.10 <- s09.10[,2:23]
+s09.10$Date <- as.Date(s09.10$Date, format = "%d/%m/%y")
 
 #/2008-2009
 s08.09 <- read.csv("2008-2009.csv")
 names(s08.09)
 s08.09 <- s08.09[,2:23]
+s08.09$Date <- as.Date(s08.09$Date, format = "%d/%m/%y")
 
 #/2007-2008
 s07.08 <- read.csv("2007-2008.csv")
 names(s07.08)
 s07.08 <- s07.08[,2:23]
+s07.08$Date <- as.Date(s07.08$Date, format = "%d/%m/%y")
 
 #/2006-2007
 s06.07 <- read.csv("2006-2007.csv")
 names(s06.07)
 s06.07 <- s06.07[,2:23]
+s06.07$Date <- as.Date(s06.07$Date, format = "%d/%m/%y")
 
 #/2005-2006
 s05.06 <- read.csv("2005-2006.csv")
 names(s05.06)
 s05.06 <- s05.06[,2:23]
+s05.06$Date <- as.Date(s05.06$Date, format = "%d/%m/%y")
 
 #/2004-2005
 s04.05 <- read.csv("2004-2005.csv")
 names(s04.05)
 s04.05 <- s04.05[,2:23]
 s04.05 <- na.omit(s04.05) #there are random empty rows, get rid of em!
+s04.05$Date <- as.Date(s04.05$Date, format = "%d/%m/%y")
 
 #/2003-2004
 s03.04 <- read.csv("2003-2004.csv")
 names(s03.04)
 s03.04 <- s03.04[,2:23]
 s03.04 <- na.omit(s03.04) #there are random empty rows, get rid of em!
+s03.04$Date <- as.Date(s03.04$Date, format = "%d/%m/%y")
 
 #/2002-2003
 s02.03 <- read.csv("2002-2003.csv")
 names(s02.03)
 s02.03 <- s02.03[,2:23]
 s02.03 <- na.omit(s02.03)
+#/02-03 has a different date format from all the rest!
+s02.03$Date <- as.Date(s02.03$Date, format = "%d/%m/%Y")
 
 #/2001-2002
 s01.02 <- read.csv("2001-2002.csv")
 names(s01.02)
 s01.02 <- s01.02[,2:30] #Select out all football stats
 s01.02 <- s01.02[,-c(10,16,17,22,23,28,29)] #now remove the ones not found in others
+s01.02$Date <- as.Date(s01.02$Date, format = "%d/%m/%y")
 
 #/2000-2001
 s00.01 <- read.csv("2000-2001.csv")
 names(s00.01)
 s00.01 <- s00.01[,2:30] #Select out all football stats 
 s00.01 <- s00.01[,-c(10,16,17,22,23,28,29)] #remove ones not found in others
+s00.01$Date <- as.Date(s00.01$Date, format = "%d/%m/%y")
 
 #NOTE: order of variables is slightly different in s01.02 and s00.01 compared to rest
 #Reverse order: HST AST HC AC HF AF
@@ -175,10 +198,12 @@ EPL[5981:6360,23] <- "01/02"
 EPL[6361:6740,23] <- "00/01"
 
 #Making a calendar year column (this will be handy later on)
-#The date column we curretly have imported as a character -- let's change that
-
-EPL$Date <- as.Date(EPL$Date,format = "%d/%m/%y")
 #mutate year column
 EPL <- EPL %>% mutate(year = Date)
 #now to save only the years
 EPL$year <- format(EPL$year,"%Y")
+
+#check
+unique(EPL$year)
+#Now let's put the matches in chronological order
+EPL <- EPL[order(EPL$Date, decreasing=T),]
