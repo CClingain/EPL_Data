dafb8728ef3eea5ddcbadc14a001808a7b39f8bb
CClingain <36608104+CClingain@users.noreply.github.com>
2018-03-04 18:51:27 -0500

    Added some plots and wilcox test

--- EPL Exploratory Analysis.R
+++ EPL Exploratory Analysis.R
@@ -12,16 +12,33 @@
 table(EPL$AY) 
 which(EPL$AY == 9)
 EPL[685,]
-#only match with 9 Away YCs (highest of any match) was the one that decided
-#that Leiceister won the league!
+#only match with 9 Away YCs (highest of any match) was the one that decided that Leiceister won the league!
 
 #Home Yellow Cards
 table(EPL$HY)
+
+#Visualizing the two Yellow Card distributions
+plot(density(EPL$AY), main = "Yellow Cards per Match 2000-2018", xlab = "# of Yellow Cards in a Match")
+lines(density(EPL$HY), col = "red")
+
+#the pretty viz!
+plot(density(EPL$HY),main = "Yellow Cards per Match 2000-2018", xlab = "# of Red Cards", lty = "solid", bty = "l", xlim=c(0,10))
+polygon(density(EPL$HY), col = rgb(red = 139, green = 0, blue = 0, alpha = 100, maxColorValue = 255), lty = "solid")
+polygon(density(EPL$AY), col = rgb(red = 0, green = 0, blue = 130, alpha = 100, maxColorValue = 255), lty = "dotted")
+legend(x = 4, y = 0.6, legend = c("Home Yellow Cards","Away Yellow Cards"),
+       lty = "solid" , col = c("red","blue"), cex=.7)
+
+mean(EPL$AY)
+mean(EPL$HY)
+median(EPL$AY)
+median(EPL$HY)
+
+#Since we don't have normality
+wilcox.test(EPL$AY, EPL$HY)
+
 
 #Away Red Cards
 table(EPL$AR)
 
 #Home Red Cards
 table(EPL$HR)
-
-
